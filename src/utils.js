7dbb646c406ed52b706c502850982ee19181a73d
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-04-14 15:25:31 -0400

    Simplify util functions.

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,25 @@
 // utils.js: utility functions
 const axios = require('axios');
 
-// build Url with apiKey and offset
-const getUrl = (apiKey, offset) => {
-  return `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&offset=${offset}`;
+/**
+ * Build url with apiKey
+ * @param {string} apiKey - API Key provided by Giphy
+ * @returns {string} URL to get Gif Objects
+ */
+const getUrl = apiKey => {
+  return `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
 };
 
-// get simple array of { id, title } object from data
-const simplifyData = data => {
-  return data
-    .filter(datum => datum.id && datum.title)
-    .map(({ id, title }) => ({ id, title }));
-};
-
-const loadData = url => {
-  return axios
-    .get(url)
-    .then(result => result.data)
-    .catch(error => console.log(error));
+/**
+ *
+ * @param {string} url - URL to get Gif Objects
+ * @param {object} params - Parameters to modify request
+ */
+const loadData = (url, params) => {
+  return axios.get(url, { params }).then(result => result.data);
 };
 
 module.exports = {
   getUrl,
   loadData,
-  simplifyData,
 };
